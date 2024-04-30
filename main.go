8e2c23b06b77b6b03a4f2c405f63fd6a610ca2d5
Ti bolom <noah.severin@ynov.com>
2024-04-30 20:26:10 +0200

    v1 log a finir

--- main.go
+++ main.go
@@ -15,16 +15,34 @@ func login(w http.ResponseWriter, r *http.Request) {
 	tmpl.Execute(w, errorLogin)
 }
 
+/*
+	func loginHandler(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "Méthode non autorisée", http.StatusMethodNotAllowed) // Code 405
+			return
+		}
+
+		username := r.FormValue("username")
+		password := r.FormValue("password")
+
+
+		isValid, err := fonction.ValidateLogin(username, password)
+		if err != nil {
+			http.Error(w, "Erreur lors de la validation du login", http.StatusInternalServerError)
+			return
+		}
+
+		if isValid {
+			http.Redirect(w, r, "/home", http.StatusSeeOther)
+		} else {
+			tmpl := template.Must(template.ParseFiles("src/templates/login.html"))
+			tmpl.Execute(w, map[string]bool{"Error": true})
+		}
+	}
+*/
 func landing(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("src/templates/landing.html"))
 	tmpl.Execute(w, errorlanding)
-}
-
-func main() {
-	http.Handle("/home", http.HandlerFunc(landing))
-	http.Handle("/login", http.HandlerFunc(login))
-	Open("http://localhost/home")
-	http.ListenAndServe("", nil)
 }
 
 func Open(url string) error {
@@ -42,4 +60,12 @@ func Open(url string) error {
 	}
 	args = append(args, url)
 	return exec.Command(cmd, args...).Start()
+}
+
+func main() {
+	http.Handle("/home", http.HandlerFunc(landing))
+	//http.HandleFunc("/login", loginHandler)
+	http.Handle("/login", http.HandlerFunc(login))
+	Open("http://localhost/home")
+	http.ListenAndServe("", nil)
 }
