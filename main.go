5313fd0c91ae03929c892031f01c7368ee76e4f3
evan.quiatol@ynov.com <148352346+qevan91@users.noreply.github.com>
2024-05-02 16:50:39 +0200

    add page only

--- main.go
+++ main.go
@@ -8,7 +8,14 @@ import (
 )
 
 var errorLogin bool = false
-var errorlanding bool = false
+var errorLanding bool = false
+var errorPost bool = false
+var errorCategories bool = false
+var errorInside bool = false
+var errorAbout bool = false
+var errorCreate bool = false
+var errorUser bool = false
+var errorParameter bool = false
 
 func login(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("src/templates/login.html"))
@@ -17,12 +24,63 @@ func login(w http.ResponseWriter, r *http.Request) {
 
 func landing(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("src/templates/landing.html"))
-	tmpl.Execute(w, errorlanding)
+	tmpl.Execute(w, errorLanding)
 }
+
+func post(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("src/templates/post.html"))
+	tmpl.Execute(w, errorPost)
+	/*input := r.Form.Get("Input")
+	if input == "Text" {
+		fmt.Println("Ya des soucis")
+	}*/
+}
+
+func categories(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("src/templates/categories.html"))
+	tmpl.Execute(w, errorCategories)
+}
+
+func inside(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("src/templates/inside.html"))
+	tmpl.Execute(w, errorInside)
+}
+
+func create(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("src/templates/newtopic.html"))
+	tmpl.Execute(w, errorCreate)
+}
+
+func about(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("src/templates/about.html"))
+	tmpl.Execute(w, errorAbout)
+}
+
+func user(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("src/templates/user.html"))
+	tmpl.Execute(w, errorUser)
+}
+
+func parameter(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("src/templates/parameter.html"))
+	tmpl.Execute(w, errorParameter)
+}
+
+/*func parameter(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("src/templates/parameter.html"))
+	tmpl.Execute(w, errorParameter)
+}*/
 
 func main() {
 	http.Handle("/home", http.HandlerFunc(landing))
 	http.Handle("/login", http.HandlerFunc(login))
+	http.Handle("/post", http.HandlerFunc(post))
+	http.Handle("/categories", http.HandlerFunc(categories))
+	http.Handle("/inside", http.HandlerFunc(inside))
+	http.Handle("/about", http.HandlerFunc(about))
+	http.Handle("/create", http.HandlerFunc(create))
+	http.Handle("/user", http.HandlerFunc(user))
+	http.Handle("/parameter", http.HandlerFunc(parameter))
 	Open("http://localhost/home")
 	http.ListenAndServe("", nil)
 }
